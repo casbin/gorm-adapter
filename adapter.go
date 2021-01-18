8243c5f636d1088a660bfe1b4c737274dc6bb3a2
Matúš Mrekaj <matus.mrekaj@icloud.com>
2021-01-18 12:53:31 +0100

    increase column size to 100
    Signed-off-by: Matúš Mrekaj <matus.mrekaj@icloud.com>

--- adapter.go
+++ adapter.go
@@ -35,13 +35,13 @@ const (
 
 type CasbinRule struct {
 	ID    uint   `gorm:"primaryKey;autoIncrement"`
-	PType string `gorm:"size:40;uniqueIndex:unique_index"`
-	V0    string `gorm:"size:40;uniqueIndex:unique_index"`
-	V1    string `gorm:"size:40;uniqueIndex:unique_index"`
-	V2    string `gorm:"size:40;uniqueIndex:unique_index"`
-	V3    string `gorm:"size:40;uniqueIndex:unique_index"`
-	V4    string `gorm:"size:40;uniqueIndex:unique_index"`
-	V5    string `gorm:"size:40;uniqueIndex:unique_index"`
+	PType string `gorm:"size:100;uniqueIndex:unique_index"`
+	V0    string `gorm:"size:100;uniqueIndex:unique_index"`
+	V1    string `gorm:"size:100;uniqueIndex:unique_index"`
+	V2    string `gorm:"size:100;uniqueIndex:unique_index"`
+	V3    string `gorm:"size:100;uniqueIndex:unique_index"`
+	V4    string `gorm:"size:100;uniqueIndex:unique_index"`
+	V5    string `gorm:"size:100;uniqueIndex:unique_index"`
 }
 
 type Filter struct {
