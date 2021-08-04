232e0dc7643313c292a938136df55d933ed1e7af
张奇峰 <1990966837@qq.com>
2021-08-04 16:29:39 +0800

    fix: PostgreSql create  index occur error
    Signed-off-by: 张奇峰 <1990966837@qq.com>

--- adapter.go
+++ adapter.go
@@ -375,7 +375,7 @@ func (a *Adapter) createTable() error {
 	}
 
 	tableName := a.getFullTableName()
-	index := "idx_" + tableName
+	index := strings.ReplaceAll("idx_"+tableName, ".", "_")
 	hasIndex := a.db.Migrator().HasIndex(t, index)
 	if !hasIndex {
 		if err := a.db.Exec(fmt.Sprintf("CREATE UNIQUE INDEX %s ON %s (ptype,v0,v1,v2,v3,v4,v5)", index, tableName)).Error; err != nil {
