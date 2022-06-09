1bd95e09d066f8f7942055654fe1ceccd4bdbbd2
dirablue ( gaku ) <dirablue@gmail.com>
2022-06-10 00:56:43 +0900

    fix: empty slice found error (#164)

--- adapter.go
+++ adapter.go
@@ -577,8 +577,10 @@ func (a *Adapter) SavePolicy(model model.Model) error {
 			}
 		}
 	}
-	if err := a.db.Create(&lines).Error; err != nil {
-		return err
+	if len(lines) > 0 {
+		if err := a.db.Create(&lines).Error; err != nil {
+			return err
+		}
 	}
 
 	return nil
